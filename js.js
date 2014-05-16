57ebccdf6e4c704ccfccfc4d19f6d4c3e4d2e7ef
Taprolano <mgiambenedetti@yahoo.com>
2014-05-16 16:45:45 +1000

    Lalala

---
+++ js.js
@@ -0,0 +1,31 @@
+$('#rooms').on('click','li',function(){
+    var app = $(this).attr('id');
+    $('#rooms').find('li').removeClass('selected');
+    $('#'+app).addClass('selected');
+    $('section').hide();
+    $('#det_'+app).show();
+});
+
+var app = angular.module('angularjs', []);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.items = [
+        { name: 'Hostel Care Unit',value: '320000'},
+        { name: 'Large Hostel Care Unit',value: '375000'},
+        { name: 'Studio Care Unit',value: '350000'},
+        { name: '2 Bedroom Unit Level One - Singles',value: '400000'},
+        { name: '2 Bedroom Unit Level One - Couples',value: '450000'},
+        { name: '2 Bedroom Unit Ground Floor - Singles',value: '425000'},
+        { name: '2 Bedroom Unit Ground Floor - Couples',value: '540000'},
+        { name: 'Dementia Care Unit',value: '350000'}];
+
+    $scope.Math = window.Math;
+
+});
+
+app.filter('isempty', function() {
+    return function(input, replaceText) {
+        if(input) return input;
+        return replaceText;
+    }
+});
